9d6de614cde93f5c0bc4d76dbc00c66ea4ea4147
sergey.g <sergey.g@sbtech.com>
2017-12-12 16:51:47 +0200

    new items

--- public/script.js
+++ public/script.js
@@ -2,98 +2,240 @@
  * Script for init map plugin
  */
 $(document).ready(function () {
-
 	/**
 	 * Variables
 	 */
 	var mapContainer = document.getElementById('js-direction-map');
 	var mapDirectionsContainer = $('.js-map-navigation');
 	var mapDirectionsItems = mapDirectionsContainer.find('a');
-
+	//проверка на наличие елемента mapContainer на странице
 	if (mapContainer) {
-
 		mapContainer.addEventListener("load", function () {
 			var svgDocument = mapContainer.contentDocument;
 			var svgElement = svgDocument.getElementsByClassName('st0');
+			var svgPin = svgDocument.getElementsByClassName('st2');
 
-			mapDirectionsItems.mouseover(function () {
-				var curName = this.getAttribute('data-name');
-
-				$(svgElement).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', 'st0 hover');
-					}
-				});
-
-				$(mapDirectionsItems).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', 'active');
-					}
-				});
-
+			var popup = document.getElementsByClassName('popup-window');
+			var triangle = document.getElementsByClassName('triangle');
+			
+			$(document).mouseup(function (e) {
+				var popupActive = $(popup).find('.popup-window');
+				if (popupActive.has(e.target).length === 0) {
+					popupActive.css({
+						'display': 'none',
+					});
+				};
+				var mapDirectionsItemClicked = $('.js-map-navigation').find('a.hover');
+				if (mapDirectionsItemClicked.has(e.target).length === 0) {
+					mapDirectionsItemClicked.removeClass('hover');
+					$(svgElement).each(function () {
+						$(this).removeClass('clicked');
+					});
+					//pin
+					$(svgPin).each(function () {
+						$(this).removeClass('clicked');
+					});
+				}
 			});
-
-			mapDirectionsItems.mouseleave(function () {
-				var curName = this.getAttribute('data-name');
-
+			//снятие эффектов при клике на область документа
+			$(document).click(function () {
+				$(popup).css({
+					'display': 'none',
+				});
+				$(svgPin).removeClass('on');
 				$(svgElement).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', 'st0');
-					}
+					this.setAttribute('class', 'st0');
 				});
-
 				$(mapDirectionsItems).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', '');
-					}
+					this.setAttribute('class', '');
 				});
-
 			});
-
+			$(svgDocument).click(function () {
+				$(popup).css({
+					'display': 'none',
+				});
+				$(svgPin).removeClass('on');
+				$(svgElement).each(function () {
+					this.setAttribute('class', 'st0');
+				});
+				$(mapDirectionsItems).each(function () {
+					this.setAttribute('class', '');
+				});
+			});
+			//событие onclick на элемент списка
+			$(mapDirectionsItems).each(function () {
+				var curName = this.getAttribute('data-name');
+				$(this).on('click', function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+					$(mapDirectionsItems).each(function () {
+						$(this).addClass('');
+						$(svgPin).removeClass('on');
+						$(popup).css({
+							'display': 'none',
+						});
+					});
+					$(this).addClass('hover');
+					$(svgElement).each(function () {
+						var mapName = this;
+						mapName.setAttribute('class', 'st0');
+						if (mapName.getAttribute('data-name') == curName) {
+							mapName.setAttribute('class', 'st0 clicked');
+						}
+					});
+					$(svgPin).removeClass('on');
+				});
+				//событие на наведение на элемент списка
+				$(this).on('mouseover', function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+					var curName = this.getAttribute('data-name');
+					$(svgElement).each(function () {
+						var mapName = this;
+						if (mapName.getAttribute('data-name') == curName && !$(mapName).hasClass('clicked')) {
+							$(this).addClass('hover');
+						}
+					});
+					//pin
+					$(svgPin).each(function () {
+						var pinName = this;
+						if (pinName.getAttribute('data-name') == curName) {
+							$(this).addClass('hover');
+						}
+					});
+				});
+				//выход с наведения
+				$(this).on('mouseleave', function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+					$(svgElement).each(function () {
+						var mapName = this;
+						if (mapName.getAttribute('data-name') == curName && !$(mapName).hasClass('clicked')) {
+							$(this).removeClass('hover');
+						}
+					});
+					//pin
+					$(svgPin).each(function () {
+						var pinName = this;
+						if (pinName.getAttribute('data-name') == curName) {
+							$(this).removeClass('hover');
+						}
+					});
+				});
+			});
+			//подсветка пунктов на наведение на карту
 			$(svgElement).mouseover(function () {
 				var curName = this.getAttribute('data-name');
-
-				$(svgElement).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', 'st0 hover');
-					}
-				});
-
 				$(mapDirectionsItems).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', 'active');
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).addClass('hover');
 					}
 				});
-
+				$(svgElement).each(function () {
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).addClass('hover');
+					}
+				});
+				$(svgPin).each(function () {
+					var pinName = this;
+					if (pinName.getAttribute('data-name') == curName) {
+						$(this).addClass('hover');
+					}
+				});
 			});
-
+			//подсветка пунктов на наведение на карту. выход
 			$(svgElement).mouseleave(function () {
 				var curName = this.getAttribute('data-name');
-
+				$(mapDirectionsItems).each(function () {
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).removeClass('hover');
+					}
+				});
+				$(svgElement).each(function () {
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).removeClass('hover');
+					}
+				});
+				$(svgPin).each(function () {
+					var pinName = this;
+					if (pinName.getAttribute('data-name') == curName) {
+						$(this).removeClass('hover');
+					}
+				});
+			});
+			//событие при клике на пин
+			$(svgPin).on('click', function (e) {
+				$(svgPin).each(function(){
+					$(svgPin).removeClass('on');
+				});
+				//появление попапа при клике на пин
+				var offsetPin = $(this).offset();
+				$(this).addClass('on');
+				var curName = this.getAttribute('data-name');
+				e.stopPropagation();
+				e.preventDefault();
+				$(popup).css({
+					'display': 'block',
+					'left': offsetPin.left - $(popup).width() / 2 + $(triangle).width() - $(svgPin).width() / 2,
+					'top': offsetPin.top - $(popup).height() - $(triangle).height() / 1.5,
+				});
+				//выделение региона на карте при клике на пин
 				$(svgElement).each(function () {
 					var mapName = this;
+					mapName.setAttribute('class', 'st0');
 					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', 'st0');
+						mapName.setAttribute('class', 'st0 clicked');
 					}
 				});
-
+				//выделение элемента списка при клике на пин
 				$(mapDirectionsItems).each(function () {
-					var mapName = this;
-					if (mapName.getAttribute('data-name') == curName) {
-						this.setAttribute('class', '');
+					var itemName = this;
+					itemName.setAttribute('class', '');
+					if (itemName.getAttribute('data-name') == curName) {
+						itemName.setAttribute('class', 'clicked');
 					}
 				});
-
 			});
-
+			//подсветка пунктов на наведение на пин
+			$(svgPin).mouseover(function () {
+				var curName = this.getAttribute('data-name');
+				$(this).addClass('hover');
+				$(mapDirectionsItems).each(function () {
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).addClass('hover');
+					}
+				});
+				$(svgElement).each(function () {
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).addClass('hover');
+					}
+				});
+			});
+			//подсветка пунктов на наведение на пин. выход
+			$(svgPin).mouseleave(function () {
+				var curName = this.getAttribute('data-name');
+				if (!$(this).hasClass('on')) {
+					$(this).removeClass('hover');
+					$(mapDirectionsItems).each(function () {
+						var itemName = this;
+						if (itemName.getAttribute('data-name') == curName) {
+							$(this).removeClass('hover');
+						}
+					});
+				}
+				$(svgElement).each(function () {
+					var itemName = this;
+					if (itemName.getAttribute('data-name') == curName) {
+						$(this).removeClass('hover');
+					}
+				});
+			});
 		}, false);
-
 	}
-
 });
--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import logo from './map.svg';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="map">
         <div className="container">
           <div className="row">
             <div className="col-md-8">
               <object id="js-direction-map" data={logo} type="image/svg+xml"></object>
+              <div className="popup-window">
+                <div className="popup-content">
+                  <p>61010, <span>м.Харків</span>,</p>
+                  <p>вул. Гольдбергівська, 63</p>
+                  <p>тел.: (057) 732-24-03</p>
+                  <p>адреса сайту філії: <a href="http://ps.ucrf.gov.ua/">http://ps.ucrf.gov.ua/</a></p>
+                  <p><a href="#">kharkov@ucrf.gov.ua</a></p>
+                </div>
+                <div className="triangle"></div>
+              </div>
             </div>
             <div className="col-md-4">
-              <div className="js-map-navigation">
-                <a href="#" data-name="symskaya">test 1</a>
-                <a href="#" data-name="chernigovskaya">test 2</a>
-                <a href="#" data-name="zaporozskaya">test 3</a>
-              </div>
+              <ul className="js-map-navigation">
+                <li><a href="#" data-name="zahidna"><span>01</span> Західна філія</a></li>
+                <li><a href="#" data-name="karpatska"><span>02</span> Карпатська філія</a></li>
+                <li><a href="#" data-name="podilska"><span>03</span> Подільська філія</a></li>
+                <li><a href="#" data-name="pivnichno-shidna"><span>04</span> Північно-Східна філія</a></li>
+                <li><a href="#" data-name="centralna"><span>05</span> Центральна філія</a></li>
+                <li><a href="#" data-name="pivdenna"><span>06</span> Південна філія</a></li>
+                <li><a href="#" data-name="donetska"><span>07</span> Донецька філія</a></li>
+                <li><a href="#" data-name="krymska"><span>08</span> Кримська філія</a></li>
+                <li><a href="#" data-name="luganska"><span>09</span> Луганська філія</a></li>
+              </ul>
             </div>
           </div>
         </div>
@@ -24,5 +39,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
