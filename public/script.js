0ab24551a984b346a310413f01ad051eeac6ee97
sergey.g <sergey.g@sbtech.com>
2017-12-12 20:45:53 +0200

    popup generator

--- public/script.js
+++ public/script.js
@@ -1,3 +1,130 @@
+
+function initView(data, container, popupStyles) {
+	var result = document.createElement('div');
+	result.classList = 'popup-content';
+	result.innerHTML = ('<p>' + data.index + ', <span>' + data.address + '</span></p>' +
+		'<p>' + data.street + '</p>' +
+		'<p>' + data.phone + '</p>' +
+		'<p>адреса сайту філії: <a href=' + data.site + ' target="_blank">' + data.site + '</a></p>' +
+		'<p><a href=mailto"' + data.email + '">' + data.email + '</a></p>');
+	console.log(result);
+	var elem = $(container)[0];
+	console.log(elem);
+	elem.innerHTML = '';
+	$(elem).append(result).append('<div class="triangle"></div>').css(popupStyles);
+}
+
+function popupGenerator(dataName, popupContainer, popupStyles) {
+
+	var popupInfo = [
+		{
+			"dataName": "zahidna",
+			"fulladdress": "м.",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "karpatska",
+			"fulladdress": "м.Харdsadasdків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "podilska",
+			"fulladdress": "м.Харdsків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "pivnichno-shidna",
+			"fulladdress": "м.Харdddків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "centralna",
+			"fulladdress": "м.Хар   ків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "pivdenna",
+			"fulladdress": "м.Харків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "donetska",
+			"fulladdress": "м.Хаfda рків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "krymska",
+			"fulladdress": "м.Хfdaрків",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		},
+		{
+			"dataName": "luganska",
+			"fulladdress": "м.Харк     ів",
+			"index": "61010",
+			"street": "вул. Гольдбергівська, 63",
+			"phone": "(057) 732-24-03",
+			"siteURL": "http://ps.ucrf.gov.ua/",
+			"email": "kharkov@ucrf.gov.ua"
+		}
+	]
+
+	var name = dataName;
+
+	for (var i = 0; i < popupInfo.length; i++) {
+		if (popupInfo[i].dataName === name) {
+			var address = popupInfo[i].fulladdress;
+			var index = popupInfo[i].index;
+			var street = popupInfo[i].street;
+			var phone = popupInfo[i].phone;
+			var site = popupInfo[i].siteURL;
+			var email = popupInfo[i].email;
+
+			var currentPopup = {
+				address: address,
+				index: index,
+				street: street,
+				phone: phone,
+				site: site,
+				email: email
+			}
+
+			initView(currentPopup, popupContainer, popupStyles);
+		}
+	}
+}
+
 /**
  * Script for init map plugin
  */
@@ -17,7 +144,7 @@ $(document).ready(function () {
 
 			var popup = document.getElementsByClassName('popup-window');
 			var triangle = document.getElementsByClassName('triangle');
-			
+
 			$(document).mouseup(function (e) {
 				var popupActive = $(popup).find('.popup-window');
 				if (popupActive.has(e.target).length === 0) {
@@ -169,7 +296,7 @@ $(document).ready(function () {
 			});
 			//событие при клике на пин
 			$(svgPin).on('click', function (e) {
-				$(svgPin).each(function(){
+				$(svgPin).each(function () {
 					$(svgPin).removeClass('on');
 				});
 				//появление попапа при клике на пин
@@ -178,11 +305,20 @@ $(document).ready(function () {
 				var curName = this.getAttribute('data-name');
 				e.stopPropagation();
 				e.preventDefault();
-				$(popup).css({
+
+				var popupStyles = {
 					'display': 'block',
 					'left': offsetPin.left - $(popup).width() / 2 + $(triangle).width() - $(svgPin).width() / 2,
-					'top': offsetPin.top - $(popup).height() - $(triangle).height() / 1.5,
-				});
+					'top': offsetPin.top - $(popup).height() - $(triangle).height() / 1.5
+				}
+
+				popupGenerator(curName, '.popup-window', popupStyles);
+
+				// $(popup).css({
+				// 	'display': 'block',
+				// 	'left': offsetPin.left - $(popup).width() / 2 + $(triangle).width() - $(svgPin).width() / 2,
+				// 	'top': offsetPin.top - $(popup).height() - $(triangle).height() / 1.5,
+				// });
 				//выделение региона на карте при клике на пин
 				$(svgElement).each(function () {
 					var mapName = this;
